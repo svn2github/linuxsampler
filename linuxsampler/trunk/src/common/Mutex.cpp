e2fde5db8ef07db4bcc2d51ee3e54880c64ba67a
senkov <senkov@d2c4447f-3961-4fff-bec3-96d84be9abe5>
2004-10-20 03:02:35 +0000

    * Quoted warnings to avoid unmatched single quote.
    git-svn-id: https://svn.linuxsampler.org/svn@290 d2c4447f-3961-4fff-bec3-96d84be9abe5

--- linuxsampler/trunk/src/common/Mutex.cpp
+++ linuxsampler/trunk/src/common/Mutex.cpp
@@ -29,9 +29,9 @@
 #if !defined(_XOPEN_SOURCE) || _XOPEN_SOURCE < 500
 # undef _XOPEN_SOURCE
 # define _XOPEN_SOURCE 500 /* to define PTHREAD_MUTEX_ERRORCHECK */
-# warning Seems you don't have a UNIX98 compatible system.
-# warning Please run LinuxSampler's selftest to make sure this won't oppose a problem!
-# warning (compile tests with 'make tests', run them with 'src/testcases/linuxsamplertest')
+# warning "Seems you don't have a UNIX98 compatible system."
+# warning "Please run LinuxSampler's selftest to make sure this won't be a problem!"
+# warning "(compile tests with 'make tests', run them with 'src/testcases/linuxsamplertest')"
 #endif
 
 #include <iostream>
