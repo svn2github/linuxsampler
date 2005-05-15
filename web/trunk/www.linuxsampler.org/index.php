186534b8aa1c9b410cd24396646f1f140ac114c3
senoner <senoner@d2c4447f-3961-4fff-bec3-96d84be9abe5>
2005-05-15 15:31:45 +0000

    Initial Revision
    git-svn-id: https://svn.linuxsampler.org/svn@542 d2c4447f-3961-4fff-bec3-96d84be9abe5

---
+++ web/trunk/www.linuxsampler.org/index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="index.css">
+  <title>The Linux Sampler Project</title>
+
+  <meta http-equiv="content-type"
+   content="text/html; charset=ISO-8859-1">
+
+   <script language="javascript" type="text/javascript">
+	<!--
+	var dir = "gfx/";
+
+	if(document.images) {
+
+		newsover = new Image();
+		newsover.src = dir + "mnewshover.png";
+
+		newsout = new Image();
+		newsout.src = dir + "mnews.png";
+
+		aboutover = new Image();
+		aboutover.src = dir + "mabouthover.png";
+
+		aboutout = new Image();
+		aboutout.src = dir + "mabout.png";
+
+		featuresover = new Image();
+		featuresover.src = dir + "mfeatureshover.png";
+
+		featuresout = new Image();
+		featuresout.src = dir + "mfeatures.png";
+
+		screenshotsover = new Image();
+		screenshotsover.src = dir + "mscreenshotshover.png";
+
+		screenshotsout = new Image();
+		screenshotsout.src = dir + "mscreenshots.png";
+
+		demosover = new Image();
+		demosover.src = dir + "mdemoshover.png";
+
+		demosout = new Image();
+		demosout.src = dir + "mdemos.png";
+
+		downloadsover = new Image();
+		downloadsover.src = dir + "mdownloadshover.png";
+
+		downloadsout = new Image();
+		downloadsout.src = dir + "mdownloads.png";
+
+		developersover = new Image();
+		developersover.src = dir + "mdevelopershover.png";
+
+		developersout = new Image();
+		developersout.src = dir + "mdevelopers.png";
+
+		instrumentsover = new Image();
+		instrumentsover.src = dir + "minstrumentshover.png";
+
+		instrumentsout = new Image();
+		instrumentsout.src = dir + "minstruments.png";
+
+		linksover = new Image();
+		linksover.src = dir + "mlinkshover.png";
+
+		linksout = new Image();
+		linksout.src = dir + "mlinks.png";
+
+		documentationover = new Image();
+		documentationover.src = dir + "mdocumentationhover.png";
+
+		documentationout = new Image();
+		documentationout.src = dir + "mdocumentation.png";
+
+		creditsover = new Image();
+		creditsover.src = dir + "mcreditshover.png";
+
+		creditsout = new Image();
+		creditsout.src = dir + "mcredits.png";
+
+		faqover = new Image();
+		faqover.src = dir + "mfaqhover.png";
+
+		faqout = new Image();
+		faqout.src = dir + "mfaq.png";
+
+		bugsover = new Image();
+		bugsover.src = dir + "mbugshover.png";
+
+		bugsout = new Image();
+		bugsout.src = dir + "mbugs.png";
+	}
+	-->
+	</script>
+
+</head>
+
+<body>
+
+	<div id="mainwindow"><img name="LinuxSampler" src="gfx/lsbackground.jpg" alt="LinuxSampler">
+
+		<h1><span class="hidden">The LinuxSampler Project</span></h1>
+
+			<table border=0 cellspacing=0 cellpadding=0><tr><th colspan=2 align=left>
+			<img name="h1blank" src="gfx/h1blank.png" alt="blank"><span class="hidden">about</span></th></tr>
+			<tr><td valign=top>
+			<a id="news" href="index.php" onmouseover="javascript:if(document.images)document.news.src = newsover.src" onmouseout="javascript:if(document.images)document.news.src = newsout.src"><img name="news" src="gfx/mnews.png" alt="news"></a>
+			<a id="about" href="about.html" onmouseover="javascript:if(document.images)document.about.src = aboutover.src" onmouseout="javascript:if(document.images)document.about.src = aboutout.src"><img name="about" src="gfx/mabout.png" alt="about"></a>
+			<a id="features" href="features.html" onmouseover="javascript:if(document.images)document.features.src = featuresover.src" onmouseout="javascript:if(document.images)document.features.src = featuresout.src"><img name="features" src="gfx/mfeatures.png" alt="features"></a>
+			<a id="screenshots" href="screenshots.html" onmouseover="javascript:if(document.images)document.screenshots.src = screenshotsover.src" onmouseout="javascript:if(document.images)document.screenshots.src = screenshotsout.src"><img name="screenshots" src="gfx/mscreenshots.png" alt="screenshots"></a>
+			<a id="demos" href="demos.html" onmouseover="javascript:if(document.images)document.demos.src = demosover.src" onmouseout="javascript:if(document.images)document.demos.src = demosout.src"><img name="demos" src="gfx/mdemos.png" alt="demos"></a>
+			<a id="downloads" href="downloads.html" onmouseover="javascript:if(document.images)document.downloads.src = downloadsover.src" onmouseout="javascript:if(document.images)document.downloads.src = downloadsout.src"><img name="downloads" src="gfx/mdownloads.png" alt="download"></a>
+			<a id="developers" href="developers.html" onmouseover="javascript:if(document.images)document.developers.src = developersover.src" onmouseout="javascript:if(document.images)document.developers.src = developersout.src"><img name="developers" src="gfx/mdevelopers.png" alt="developers"></a>
+			<a id="faq" href="faq.html" onmouseover="javascript:if(document.images)document.faq.src = faqover.src" onmouseout="javascript:if(document.images)document.faq.src = faqout.src"><img name="faq" src="gfx/mfaq.png" alt="faq"></a>
+			<a id="documentation" href="documentation.html" onmouseover="javascript:if(document.images)document.documentation.src = documentationover.src" onmouseout="javascript:if(document.images)document.documentation.src = documentationout.src"><img name="documentation" src="gfx/mdocumentation.png" alt="documentation"></a>
+			<a id="bugs" href="http://bugs.linuxsampler.org" onmouseover="javascript:if(document.images)document.bugs.src = bugsover.src" onmouseout="javascript:if(document.images)document.bugs.src = bugsout.src"><img name="bugs" src="gfx/mbugs.png" alt="bugs"></a>
+			<a id="instruments" href="instruments.html" onmouseover="javascript:if(document.images)document.instruments.src = instrumentsover.src" onmouseout="javascript:if(document.images)document.instruments.src = instrumentsout.src"><img name="instruments" src="gfx/minstruments.png" alt="instruments"></a>
+			<a id="links" href="links.html" onmouseover="javascript:if(document.images)document.links.src = linksover.src" onmouseout="javascript:if(document.images)document.links.src = linksout.src"><img name="links" src="gfx/mlinks.png" alt="links"></a>
+			<a id="credits" href="credits.html" onmouseover="javascript:if(document.images)document.credits.src = creditsover.src" onmouseout="javascript:if(document.images)document.credits.src = creditsout.src"><img name="credits" src="gfx/mcredits.png" alt="credits"></a>
+			</td><td valign=top width="100%">
+			<div id="contents">
+
+			<h2>News</h2>
+
+            <table>
+              <tbody>
+                <tr>
+                  <td valign=top>
+                    <p class="default">
+		      <b>2005-03-15</b> Our bug tracking system is now online and available on
+			  <a href="http://bugs.linuxsampler.org">bugs.linuxsampler.org</a>.<br>
+		      <br>
+		      <b>2005-03-04</b> <a href="http://qsampler.sourceforge.net">QSampler</a>,
+		      the Qt GUI front-end in the works for the LinuxSampler engine, was bumped
+		      to its now fifth primordial release (0.0.5). Still alpha code. Time to move on...
+		      to a better and complete Audio/MIDI device configuration interface, as predicted
+		      by LSCP and wrapped by liblscp.<br>
+		      <br>
+		      <b>2005-02-26</b> Due to a design change, current LinuxSampler CVS version is quite
+		      unstable. You might want to use the previous, stable version of LS in the meantime,
+		      which can be downloaded as <a href="http://www.linuxsampler.org/downloads.html">source tarball</a>.
+		      Alternatively you can also checkout the old version from CVS which was tagged as "v0_2_0".<br>
+		      <br>
+		      <b>2004-11-19</b> The Qt GUI front-end for the LinuxSampler engine has been updated: the 4th alpha-release of
+		      <a href="http://qsampler.sourceforge.net">QSampler</a> has been released.<br>
+		      <br>
+		      <b>2004-11-13</b> On monday (November, 15th) <a href="http://cvs.linuxsampler.org">cvs.linuxsampler.org</a>
+		      may be temporarily down due to maintenance work around 13:00 UTC.<br>
+		      <br>
+		      <b>2004-07-06</b> The third alpha-release of <a href="http://qsampler.sourceforge.net">QSampler</a>,
+		      the Qt GUI front-end for the LinuxSampler engine, is now publicly available.<br>
+		      <br>
+		      <b>2004-06-05</b> Added a screenshot to the Screenshots page, instructions how to compile the GUI are on the linked qsampler site.<br>
+		      <br>
+		      <b>2004-05-31</b> On wednesday (June, 2nd) <a href="http://cvs.linuxsampler.org">cvs.linuxsampler.org</a> will
+		      be temporarily not available from 6:00 UTC until 16:00 UTC due to construction works.<br>
+		      <br>
+		      <b>2004-05-24</b> The CVS repository is now <a href="http://cvs.linuxsampler.org">browseable</a>.<br>
+		      <br>
+		      <b>2004-05-03</b> We are currently in a transition stage due to the recent changes
+		      (multi channel / multi engine / multi audio output device and multi MIDI input device
+		      support) this also leads to multi problems we have to solve ;-) so meanwhile you
+		      can use the old single channel version. We created the two CVS alias tags
+		      'singlechannel' and 'v0_1_0' which you can use to checkout the old single channel
+		      version of LinuxSampler . The CVS command line is the same except that you have to add
+		      '-r singlechannel'.<br>
+		      <br>
+		      <b>2004-03-26</b> CVS root path has changed! See <a href="downloads.html">downloads</a> section
+		      for instructions how to checkout the latest version of LinuxSampler from CVS.<br>
+		      <br>
+		      <b>2004-03-23</b> Updated <a href="features.html">features</a> section on this site,
+		      hopefully we will keep the features list in sync with the latest developments from now on.<br>
+		      <br>
+                      <b>2004-01-07</b> The document for the network protocol definition is now available in the <a style="text-decoration: none;" href="http://www.linuxsampler.org/downloads.html">downloads</a> section. Have a look at it if you can imagine to write a frontend for LinuxSampler!<br>
+  		      <br>
+                      <b>2003-11-08</b> Added CVS log to this site, showing the latest commits to the LinuxSampler CVS repository.<br>
+  			          <br>
+  			          <b>2003-11-02</b> Lots of news for the Linuxsampler community. We've got a new site, new domain and new code available in CVS. Check the <a style="text-decoration: none;" href="http://www.linuxsampler.org/downloads.html">downloads</a> section for instructions. If you want to check it out, please keep in mind that it might contain bugs, it might crash and that it's still lacking tons of features. CVS is directed towards developers who would like to test things and add code. Users should wait for a tarball release.
+  			          <br>
+			        </p>
+                  </td>
+                  <td valign="top">
+
+
+                    <table>
+                      <thead>
+                        <tr>
+                          <th scope=col><b>Latest Commits LinuxSampler (Engine)</b></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>
+			    <span class="default">
+			      <?php include("http://cvs.linuxsampler.org/log/linuxsampler_latest.html"); ?>
+			    </span>
+			  </td>
+			</tr>
+                        <tr>
+                          <td><a href="http://www.linuxsampler.org/linuxsampler_cvshistory.php">More... (LinuxSampler CVS History)</a></td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+		    <br>
+
+                    <table>
+                      <thead>
+                        <tr>
+                          <th scope=col><b>Latest Commits QSampler (GUI)</b></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+			<tr>
+			  <td>
+			    <span class="default">
+                              <?php include("http://cvs.linuxsampler.org/log/qsampler_latest.html"); ?>
+			    </span>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td><a href="http://www.linuxsampler.org/qsampler_cvshistory.php">More... (QSampler CVS History)</a></td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+		    <br>
+
+
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+
+	  	</div>
+	  	</td></tr></table>
+
+ </div>
+
+</body>
+</html>
---
+++ web/trunk/www.linuxsampler.org/qsampler_cvshistory.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="index.css">
+  <title>The Linux Sampler Project</title>
+
+  <meta http-equiv="content-type"
+   content="text/html; charset=ISO-8859-1">
+
+   <script language="javascript" type="text/javascript">
+	<!--
+	var dir = "gfx/";
+
+	if(document.images) {
+
+		newsover = new Image();
+		newsover.src = dir + "mnewshover.png";
+
+		newsout = new Image();
+		newsout.src = dir + "mnews.png";
+
+		aboutover = new Image();
+		aboutover.src = dir + "mabouthover.png";
+
+		aboutout = new Image();
+		aboutout.src = dir + "mabout.png";
+
+		featuresover = new Image();
+		featuresover.src = dir + "mfeatureshover.png";
+
+		featuresout = new Image();
+		featuresout.src = dir + "mfeatures.png";
+
+		screenshotsover = new Image();
+		screenshotsover.src = dir + "mscreenshotshover.png";
+
+		screenshotsout = new Image();
+		screenshotsout.src = dir + "mscreenshots.png";
+
+		demosover = new Image();
+		demosover.src = dir + "mdemoshover.png";
+
+		demosout = new Image();
+		demosout.src = dir + "mdemos.png";
+
+		downloadsover = new Image();
+		downloadsover.src = dir + "mdownloadshover.png";
+
+		downloadsout = new Image();
+		downloadsout.src = dir + "mdownloads.png";
+
+		developersover = new Image();
+		developersover.src = dir + "mdevelopershover.png";
+
+		developersout = new Image();
+		developersout.src = dir + "mdevelopers.png";
+
+		instrumentsover = new Image();
+		instrumentsover.src = dir + "minstrumentshover.png";
+
+		instrumentsout = new Image();
+		instrumentsout.src = dir + "minstruments.png";
+
+		linksover = new Image();
+		linksover.src = dir + "mlinkshover.png";
+
+		linksout = new Image();
+		linksout.src = dir + "mlinks.png";
+
+		documentationover = new Image();
+		documentationover.src = dir + "mdocumentationhover.png";
+
+		documentationout = new Image();
+		documentationout.src = dir + "mdocumentation.png";
+
+		creditsover = new Image();
+		creditsover.src = dir + "mcreditshover.png";
+
+		creditsout = new Image();
+		creditsout.src = dir + "mcredits.png";
+
+		bugsover = new Image();
+		bugsover.src = dir + "mbugshover.png";
+
+		bugsout = new Image();
+		bugsout.src = dir + "mbugs.png";
+	}
+	-->
+	</script>
+
+</head>
+
+<body>
+
+	<div id="mainwindow"><img name="LinuxSampler" src="gfx/lsbackground.jpg" alt="LinuxSampler">
+
+		<h1><span class="hidden">The LinuxSampler Project</span></h1>
+
+			<table border=0 cellspacing=0 cellpadding=0><tr><th colspan=2 align=left>
+			<img name="h1blank" src="gfx/h1blank.png" alt="h1blank"><span class="hidden">about</span></th></tr>
+			<tr><td valign=top>
+			<a id="news" href="index.php" onmouseover="javascript:if(document.images)document.news.src = newsover.src" onmouseout="javascript:if(document.images)document.news.src = newsout.src"><img name="news" src="gfx/mnews.png" alt="news"></a>
+			<a id="about" href="about.html" onmouseover="javascript:if(document.images)document.about.src = aboutover.src" onmouseout="javascript:if(document.images)document.about.src = aboutout.src"><img name="about" src="gfx/mabout.png" alt="about"></a>
+			<a id="features" href="features.html" onmouseover="javascript:if(document.images)document.features.src = featuresover.src" onmouseout="javascript:if(document.images)document.features.src = featuresout.src"><img name="features" src="gfx/mfeatures.png" alt="features"></a>
+			<a id="screenshots" href="screenshots.html" onmouseover="javascript:if(document.images)document.screenshots.src = screenshotsover.src" onmouseout="javascript:if(document.images)document.screenshots.src = screenshotsout.src"><img name="screenshots" src="gfx/mscreenshots.png" alt="screenshots"></a>
+			<a id="demos" href="demos.html" onmouseover="javascript:if(document.images)document.demos.src = demosover.src" onmouseout="javascript:if(document.images)document.demos.src = demosout.src"><img name="demos" src="gfx/mdemos.png" alt="demos"></a>
+			<a id="downloads" href="downloads.html" onmouseover="javascript:if(document.images)document.downloads.src = downloadsover.src" onmouseout="javascript:if(document.images)document.downloads.src = downloadsout.src"><img name="downloads" src="gfx/mdownloads.png" alt="download"></a>
+			<a id="developers" href="developers.html" onmouseover="javascript:if(document.images)document.developers.src = developersover.src" onmouseout="javascript:if(document.images)document.developers.src = developersout.src"><img name="developers" src="gfx/mdevelopers.png" alt="developers"></a>
+			<a id="faq" href="faq.html" onmouseover="javascript:if(document.images)document.faq.src = faqover.src" onmouseout="javascript:if(document.images)document.faq.src = faqout.src"><img name="faq" src="gfx/mfaq.png" alt="faq"></a>
+			<a id="documentation" href="documentation.html" onmouseover="javascript:if(document.images)document.documentation.src = documentationover.src" onmouseout="javascript:if(document.images)document.documentation.src = documentationout.src"><img name="documentation" src="gfx/mdocumentation.png" alt="documentation"></a>
+			<a id="bugs" href="http://bugs.linuxsampler.org" onmouseover="javascript:if(document.images)document.bugs.src = bugsover.src" onmouseout="javascript:if(document.images)document.bugs.src = bugsout.src"><img name="bugs" src="gfx/mbugs.png" alt="bugs"></a>
+			<a id="instruments" href="instruments.html" onmouseover="javascript:if(document.images)document.instruments.src = instrumentsover.src" onmouseout="javascript:if(document.images)document.instruments.src = instrumentsout.src"><img name="instruments" src="gfx/minstruments.png" alt="instruments"></a>
+			<a id="links" href="links.html" onmouseover="javascript:if(document.images)document.links.src = linksover.src" onmouseout="javascript:if(document.images)document.links.src = linksout.src"><img name="links" src="gfx/mlinks.png" alt="links"></a>
+			<a id="credits" href="credits.html" onmouseover="javascript:if(document.images)document.credits.src = creditsover.src" onmouseout="javascript:if(document.images)document.credits.src = creditsout.src"><img name="credits" src="gfx/mcredits.png" alt="credits"></a>
+			</td><td valign=top width="100%">
+			<div id="contents">
+
+			<h2>QSampler CVS History</h2>
+
+                        <p class="default">
+                          <?php include("http://cvs.linuxsampler.org/log/qsampler_history.html"); ?>
+                          <br>
+                        </p>
+
+	  	</div>
+	  	</td></tr></table>
+
+ </div>
+
+</body>
+</html>
