3b808083bdb848a49b2bd8547bdf98ddec9a5712
schoenebeck <schoenebeck@d2c4447f-3961-4fff-bec3-96d84be9abe5>
2005-11-12 12:36:49 +0000

    forgot to add new header 'helper.h' recently
    git-svn-id: https://svn.linuxsampler.org/svn@803 d2c4447f-3961-4fff-bec3-96d84be9abe5

---
+++ libgig/trunk/src/helper.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *                                                                         *
+ *   libgig - C++ cross-platform Gigasampler format file loader library    *
+ *                                                                         *
+ *   Copyright (C) 2003-2005 by Christian Schoenebeck                      *
+ *                              <cuse@users.sourceforge.net>               *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this library; if not, write to the Free Software           *
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston,                 *
+ *   MA  02111-1307  USA                                                   *
+ ***************************************************************************/
+
+#ifndef __LIBGIG_HELPER_H__
+#define __LIBGIG_HELPER_H__
+
+#include <string>
+#include <sstream>
+
+// *************** Helper Functions **************
+// *
+
+template<class T> inline std::string ToString(T o) {
+    std::stringstream ss;
+    ss << o;
+    return ss.str();
+}
+
+#endif // __LIBGIG_HELPER_H__
