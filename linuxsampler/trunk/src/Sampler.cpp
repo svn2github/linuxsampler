3e0efc97db9d875f276867dee5e00df2c77ee7b8
schoenebeck <schoenebeck@d2c4447f-3961-4fff-bec3-96d84be9abe5>
2007-01-03 17:49:44 +0000

    * delete all MIDI instrument maps when a sampler-reset is issued   (i.e. on a "RESET" LSCP command)
    git-svn-id: https://svn.linuxsampler.org/svn@1008 d2c4447f-3961-4fff-bec3-96d84be9abe5

--- linuxsampler/trunk/src/Sampler.cpp
+++ linuxsampler/trunk/src/Sampler.cpp
@@ -3,20 +3,20 @@
  *   LinuxSampler - modular, streaming capable sampler                     *
  *                                                                         *
  *   Copyright (C) 2003, 2004 by Benno Senoner and Christian Schoenebeck   *
- *   Copyright (C) 2005, 2006 Christian Schoenebeck                        *
+ *   Copyright (C) 2005 - 2007 Christian Schoenebeck                       *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
+ *   This library is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
+ *   This library is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the Free Software           *
+ *   along with this library; if not, write to the Free Software           *
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston,                 *
  *   MA  02111-1307  USA                                                   *
  ***************************************************************************/
@@ -29,6 +29,7 @@
 #include "engines/EngineChannelFactory.h"
 #include "drivers/audio/AudioOutputDeviceFactory.h"
 #include "drivers/midi/MidiInputDeviceFactory.h"
+#include "drivers/midi/MidiInstrumentMapper.h"
 #include "network/lscpserver.h"
 
 namespace LinuxSampler {
@@ -441,6 +442,15 @@ namespace LinuxSampler {
         }
         catch(...) {
             std::cerr << "Sampler::Reset(): Exception occured while trying to delete all audio output devices, exiting.\n" << std::flush;
+            exit(EXIT_FAILURE);
+        }
+
+        // delete MIDI instrument maps
+        try {
+            MidiInstrumentMapper::RemoveAllMaps();
+        }
+        catch(...) {
+            std::cerr << "Sampler::Reset(): Exception occured while trying to delete all MIDI instrument maps, exiting.\n" << std::flush;
             exit(EXIT_FAILURE);
         }
     }
