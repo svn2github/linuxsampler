e0b30c36f1fe8c854d631c88aebbb6094f104eaf
schoenebeck <schoenebeck@d2c4447f-3961-4fff-bec3-96d84be9abe5>
2007-03-04 14:49:30 +0000

    - just minor automake / autoconf fix (moved check for intltool from   configure.ac to autogen.sh, since intltool is only needed at autogen.sh   stage anyway)
    git-svn-id: https://svn.linuxsampler.org/svn@1066 d2c4447f-3961-4fff-bec3-96d84be9abe5

--- gigedit/trunk/autogen.sh
+++ gigedit/trunk/autogen.sh
@@ -1,3 +1,30 @@
+#!/bin/sh
+
+if ! which glib-gettextize > /dev/null ; then
+  echo "Could not find glib-gettextize, make sure libglib2.0(-dev) is installed!"
+  exit -1
+fi
+if ! which intltoolize > /dev/null ; then
+  echo "Could not find intltoolize, make sure intltool is installed!"
+  exit -1
+fi
+if ! which aclocal > /dev/null ; then
+  echo "Could not find aclocal, make sure automake is installed!"
+  exit -1
+fi
+if ! which autoconf > /dev/null ; then
+  echo "Could not find autoconf, make sure it is installed!"
+  exit -1
+fi
+if ! which autoheader > /dev/null ; then
+  echo "Could not find autoheader, make sure autoconf is installed!"
+  exit -1
+fi
+if ! which automake > /dev/null ; then
+  echo "Could not find automake, make sure it is installed!"
+  exit -1
+fi
+
 glib-gettextize --force
 intltoolize --force --automake
 aclocal
