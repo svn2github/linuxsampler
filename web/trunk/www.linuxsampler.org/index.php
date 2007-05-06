fdeff50655a7a21e3055d810afa14adacb046d24
capela <capela@d2c4447f-3961-4fff-bec3-96d84be9abe5>
2007-05-06 13:20:52 +0000

    Missing link on Qsampler 0.1.4 announcement.
    git-svn-id: https://svn.linuxsampler.org/svn@1171 d2c4447f-3961-4fff-bec3-96d84be9abe5

--- web/trunk/www.linuxsampler.org/index.php
+++ web/trunk/www.linuxsampler.org/index.php
@@ -129,7 +129,7 @@
                 <tr>
                   <td valign=top>
                     <p class="default">
-              <b>2007-05-04</b> Qsampler 0.1.4 has been released. Almost another year has gone by.<br>
+              <b>2007-05-04</b> <a href="http://qsampler.sourceforge.net">Qsampler</a> 0.1.4 has been released. Almost another year has gone by.<br>
               <br>
               <b>2007-04-02</b> JSampler 0.4a has been released. Read the <a href="jsampler/manual/html/jsampler.html">manual</a>
               for installation instructions.<br>
