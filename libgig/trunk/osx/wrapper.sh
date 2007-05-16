7f9afb02419d41b5964ee1a618d75b2fac00d403
schoenebeck <schoenebeck@d2c4447f-3961-4fff-bec3-96d84be9abe5>
2007-05-16 20:38:55 +0000

    * XCode project file(s) now capable of creating universal binaries for OSX
    git-svn-id: https://svn.linuxsampler.org/svn@1190 d2c4447f-3961-4fff-bec3-96d84be9abe5

--- libgig/trunk/osx/wrapper.sh
+++ libgig/trunk/osx/wrapper.sh
@@ -2,4 +2,5 @@ export BUILD_BASE_DIR=$PWD/../temp_build
 export WITH_INSTALL=1
 export CONFIG_OPTIONS="--disable-shared"
 export PKG_CONFIG_PATH="$BUILD_BASE_DIR/$BUILD_STYLE/local/lib/pkgconfig"
+export UB_PRODUCTS="bin/dlsdump bin/gigextract bin/rifftree bin/gigdump lib/libgig.a"
 source $PROJECT_DIR/autoconf_builder.sh
