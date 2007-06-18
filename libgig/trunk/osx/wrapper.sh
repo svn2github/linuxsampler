da25881e9ddbeb774fd6a3a2dd11df5724447398
schoenebeck <schoenebeck@d2c4447f-3961-4fff-bec3-96d84be9abe5>
2007-06-18 08:04:37 +0000

    - lib needs to be linked against CoreFoundation (on OSX) for the latest   DLSID (uuid) generation support (patch by Toshi Nagata)
    git-svn-id: https://svn.linuxsampler.org/svn@1243 d2c4447f-3961-4fff-bec3-96d84be9abe5

--- libgig/trunk/osx/wrapper.sh
+++ libgig/trunk/osx/wrapper.sh
@@ -3,4 +3,6 @@ export WITH_INSTALL=1
 export CONFIG_OPTIONS="--disable-shared"
 export PKG_CONFIG_PATH="$BUILD_BASE_DIR/$BUILD_STYLE/local/lib/pkgconfig"
 export UB_PRODUCTS="bin/dlsdump bin/gigextract bin/rifftree bin/gigdump lib/libgig.a"
+export CFLAGS="-framework CoreFoundation"
+export CXXFLAGS="$CFLAGS"
 source $PROJECT_DIR/autoconf_builder.sh
