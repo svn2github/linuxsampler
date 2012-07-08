29de2d09bccb4d1791f9756f7a7a3faa30a4f675
persson <persson@d2c4447f-3961-4fff-bec3-96d84be9abe5>
2012-07-08 14:06:53 +0000

    - forgot to update copyright years in last commit
    git-svn-id: https://svn.linuxsampler.org/svn@2355 d2c4447f-3961-4fff-bec3-96d84be9abe5

--- linuxsampler/trunk/src/hostplugins/dssi/PluginDssi.cpp
+++ linuxsampler/trunk/src/hostplugins/dssi/PluginDssi.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *                                                                         *
- *   Copyright (C) 2008 - 2011 Andreas Persson                             *
+ *   Copyright (C) 2008 - 2012 Andreas Persson                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
