5337fa90ae73d0af49710d1dbd124db412dc89a8
schoenebeck <schoenebeck@d2c4447f-3961-4fff-bec3-96d84be9abe5>
2016-04-26 17:42:26 +0000

    * User settings are now automatically saved and restored. * Bumped version (1.0.0.svn5).
    git-svn-id: https://svn.linuxsampler.org/svn@2891 d2c4447f-3961-4fff-bec3-96d84be9abe5

--- gigedit/trunk/src/gigedit/Settings.cpp
+++ gigedit/trunk/src/gigedit/Settings.cpp
@@ -1,20 +1,138 @@
 /*
-    Copyright (c) 2014-2015 Christian Schoenebeck
+    Copyright (c) 2014-2016 Christian Schoenebeck
     
     This file is part of "gigedit" and released under the terms of the
     GNU General Public License version 2.
 */
 
 #include "Settings.h"
+#include <glib.h>
+#include "global.h"
+#include <glibmm/keyfile.h>
+#include <iostream>
+#include <stdio.h>
 
-static Settings _instance;
+static std::string configDir() {
+    //printf("configDir '%s'\n", g_get_user_config_dir());
+    return g_get_user_config_dir();
+}
+
+static std::string dirSep() {
+    //printf("sep '%s'\n", G_DIR_SEPARATOR_S);
+    return G_DIR_SEPARATOR_S;
+}
+
+static std::string configFile() {
+    return configDir() + dirSep() + "gigedit.conf";
+}
+
+static std::string groupName(Settings::Group_t group) {
+    return "Global";
+}
+
+static Settings* _instance = NULL;
     
 Settings* Settings::singleton() {
-    return &_instance;
+    if (!_instance) {
+        _instance = new Settings;
+        _instance->load();
+    }
+    return _instance;
 }
 
-Settings::Settings() {
-    warnUserOnExtensions = true;
-    syncSamplerInstrumentSelection = true;
-    moveRootNoteWithRegionMoved = true;
+Settings::Settings() : Glib::ObjectBase(typeid(Settings)),
+    warnUserOnExtensions(*this, GLOBAL, "warnUserOnExtensions", true),
+    syncSamplerInstrumentSelection(*this, GLOBAL, "syncSamplerInstrumentSelection", true),
+    moveRootNoteWithRegionMoved(*this, GLOBAL, "moveRootNoteWithRegionMoved", true),
+    m_ignoreNotifies(false)
+{
+    m_boolProps.push_back(&warnUserOnExtensions);
+    m_boolProps.push_back(&syncSamplerInstrumentSelection);
+    m_boolProps.push_back(&moveRootNoteWithRegionMoved);
 }
+
+void Settings::onPropertyChanged(Glib::PropertyBase* pProperty, RawValueType_t type, Group_t group) {
+    if (m_ignoreNotifies) return;
+
+    //printf("Settings::onPropertyChanged(%s)\n", pProperty->get_name().c_str());
+
+    Glib::KeyFile file;
+    try {
+        bool ok = file.load_from_file(configFile());
+        if (!ok) {
+            std::cerr << "Could not load '" << configFile() << "'\n" << std::flush;
+        }
+    } catch (...) {
+        std::cerr << "Could not load '" << configFile() << "'\n" << std::flush;
+    }
+
+    switch (type) {
+        case BOOLEAN: {
+            Property<bool>* prop = static_cast<Property<bool>*>(pProperty);
+            //std::cout << "Saving bool setting '" << prop->get_name() << "'\n" << std::flush;
+            file.set_boolean(groupName(prop->group()), prop->get_name(), prop->get_value());
+            break;
+        }
+        case INTEGER: {
+            Property<int>* prop = static_cast<Property<int>*>(pProperty);
+            //std::cout << "Saving int setting '" << prop->get_name() << "'\n" << std::flush;
+            file.set_integer(groupName(prop->group()), prop->get_name(), prop->get_value());
+            break;
+        }
+        case UNKNOWN:
+            std::cerr << "BUG: Unknown setting raw type of property '" << pProperty->get_name() << "'\n" << std::flush;
+            return;
+    }
+
+    try {
+        bool ok = file.save_to_file(configFile());
+        if (!ok) {
+            std::cerr << "Failed saving gigedit config to '" << configFile() << "'\n" << std::flush;
+        } else {
+            //std::cout <<"gigedit CONFIG SAVED\n";
+        }
+    } catch (...) {
+        std::cerr << "Failed saving gigedit config to '" << configFile() << "'\n" << std::flush;
+    }
+}
+
+void Settings::load() {
+    Glib::KeyFile file;
+    try {
+        bool ok = file.load_from_file(configFile());
+        if (!ok) return;
+    } catch (...) {
+        std::cerr << "Could not load gigedit config file '" << configFile() << "'\n" << std::flush;
+        return;
+    }
+
+    // ignore onPropertyChanged() calls during updating the property values below
+    m_ignoreNotifies = true;
+
+    for (int i = 0; i < m_boolProps.size(); ++i) {
+        Property<bool>* prop = static_cast<Property<bool>*>(m_boolProps[i]);
+        try {
+            const std::string group = groupName(prop->group());
+            if (!file.has_key(group, prop->get_name())) continue;
+            const bool value = file.get_boolean(group, prop->get_name());
+            prop->set_value(value);
+        } catch (...) {
+            continue;
+        }
+    }
+
+    for (int i = 0; i < m_intProps.size(); ++i) {
+        Property<int>* prop = static_cast<Property<int>*>(m_intProps[i]);
+        try {
+            const std::string group = groupName(prop->group());
+            if (!file.has_key(group, prop->get_name())) continue;
+            const int value = file.get_integer(group, prop->get_name());
+            prop->set_value(value);
+        } catch (...) {
+            continue;
+        }
+    }
+
+    m_ignoreNotifies = false;
+}
+
--- gigedit/trunk/src/gigedit/Settings.h
+++ gigedit/trunk/src/gigedit/Settings.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2014-2015 Christian Schoenebeck
+    Copyright (c) 2014-2016 Christian Schoenebeck
     
     This file is part of "gigedit" and released under the terms of the
     GNU General Public License version 2.
@@ -8,16 +8,121 @@
 #ifndef GIGEDIT_SETTINGS
 #define GIGEDIT_SETTINGS
 
-// Probably a bit overkill for only few settings (right now), but I saw
-// originnaly no better place to put this, since warnUserOnExtensions needs to
-// be accessed without a MainWindow instance.
-struct Settings {
-    bool warnUserOnExtensions; ///< if enabled, the user shall he be warned if he is trying to use a gig format extension that will not work with Gigasampler/GigaStudio
-    bool syncSamplerInstrumentSelection; ///< if enabled, the sampler's current instrument will automatically be switched whenever another instrument was selected in gigedit
-    bool moveRootNoteWithRegionMoved; ///< if enabled, the root note(s) of regions are automatically moving when the user drags a region around at the virtual keyboard
+#include <typeinfo>
+#include <glibmm/object.h>
+#include <glibmm/property.h>
+#include <vector>
+
+/**
+ * Reflects, saves and restores all settings for the gigedit application.
+ *
+ * This class holds a bunch of custom Property objects which can be accessed
+ * as if they were basic data types (i.e. by using assignment operator, etc.).
+ * As soon as a property gets modified this way, it will automatically be saved
+ * to a local config file.
+ */
+class Settings : public Glib::Object {
+public:
+    /**
+     * Data types for the individual settings.
+     */
+    enum RawValueType_t {
+        BOOLEAN,
+        INTEGER,
+        UNKNOWN
+    };
+
+    /**
+     * All settings are grouped into these settings groups.
+     */
+    enum Group_t {
+        GLOBAL
+    };
+
+    /**
+     * Extension of regular Glib::Property template class; this one
+     * automatically calls Settings::onPropertyChanged() method passing this
+     * property object as pointer; and it allows to assign new values to this
+     * property by using the regular assignment operator; and requires a
+     * "Setting group" to be assigned to the property at construction time.
+     */
+    template<typename T>
+    class Property : public Glib::Property<T> {
+    public:
+        Property(Settings& object, Group_t group, const Glib::ustring& name)
+            : Glib::Property<T>::Property(object, name)
+        {
+            m_settings = &object;
+            m_group = group;
+            const RawValueType_t type = rawValueType();
+            Glib::Property<T>::get_proxy().signal_changed().connect(
+                sigc::bind(
+                    sigc::bind(
+                        sigc::bind(
+                            sigc::mem_fun(m_settings, &Settings::onPropertyChanged),
+                            m_group
+                        ),
+                        type
+                    ),
+                    this
+                )
+            );
+        }
+
+        Property(Settings& object, Group_t group, const Glib::ustring& name, const T& default_value)
+            : Glib::Property<T>::Property(object, name, default_value)
+        {
+            m_settings = &object;
+            m_group = group;
+            const RawValueType_t type = rawValueType();
+            Glib::Property<T>::get_proxy().signal_changed().connect(
+                sigc::bind(
+                    sigc::bind(
+                        sigc::bind(
+                            sigc::mem_fun(m_settings, &Settings::onPropertyChanged),
+                            m_group
+                        ),
+                        type
+                    ),
+                    this
+                )
+            );
+        }
+
+        Property<T>& operator=(const T value) {
+            Glib::Property<T>::set_value(value);
+            return *this;
+        }
+
+        RawValueType_t rawValueType() const {
+            const std::string name = typeid(T).name();
+            if (name == "bool" || name == "b") return BOOLEAN;
+            if (name == "int" || name == "i") return INTEGER;
+            return UNKNOWN;
+        }
+
+        Group_t group() const { return m_group; }
+
+    private:
+        Settings* m_settings;
+        Group_t m_group;
+    };
+
+    Property<bool> warnUserOnExtensions; ///< if enabled, the user shall he be warned if he is trying to use a gig format extension that will not work with Gigasampler/GigaStudio
+    Property<bool> syncSamplerInstrumentSelection; ///< if enabled, the sampler's current instrument will automatically be switched whenever another instrument was selected in gigedit
+    Property<bool> moveRootNoteWithRegionMoved; ///< if enabled, the root note(s) of regions are automatically moving when the user drags a region around at the virtual keyboard
 
     static Settings* singleton();
     Settings();
+    void load();
+
+protected:
+    void onPropertyChanged(Glib::PropertyBase* pProperty, RawValueType_t type, Group_t group);
+
+private:
+    std::vector<Glib::PropertyBase*> m_boolProps; ///< Pointers to all 'bool' type properties this Setting class manages.
+    std::vector<Glib::PropertyBase*> m_intProps; ///< Pointers to all 'int' type properties this Setting class manages.
+    bool m_ignoreNotifies;
 };
 
 #endif // GIGEDIT_SETTINGS
